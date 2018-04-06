482118c6ecbfdc2e09161877db8ae3a5dac0fdda
Martin Kopec <xkopec42@gmail.com>
2018-04-06 11:36:53 +0200

    Load tiff image, install requirements

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
 import argparse
+from PIL import Image
+# import sys
+# import numpy
+
+
+def load_tiff_image(filepath):
+    img = Image.open(filepath)
+
+    w = img.size[0]
+    h = img.size[1]
+
+    pix = img.load()
+    print(w, h, pix)
+    return pix
 
 
 def parse_args():
@@ -13,3 +27,4 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     print(args)
+    load_tiff_image(args.image_path)
