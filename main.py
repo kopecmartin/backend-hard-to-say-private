11b1523e886790f3c4b7ba6d2ed666c2f6c49889
Maros Kopec <makopec@redhat.com>
2018-04-06 11:03:42 +0200

    Parse arguments

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image-path", required=True)
+    parser.add_argument("--csv-path", required=True)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    print("hello world")
+    args = parse_args()
+    print(args)
