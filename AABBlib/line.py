8545fa87c367957ad19948c16e59cdc48cf9b043
Martin Kopec <xkopec42@gmail.com>
2018-04-06 19:08:59 +0200

    fix typos

--- AABBlib/line.py
+++ AABBlib/line.py
@@ -9,11 +9,11 @@ def get_line_eq(x1, y1, x2, y2):
     return {'a':(y1-y2), 'b':(x2-x1), 'c':(x1*y2-x2*y1)}
 
 
-def is_on_normal(line_eq, normal_eq):
-    """ Returns True if given line is normal to given line equation
+def is_normal(line_eq, normal_eq):
+    """ True if lines are orthogonal
     """
     if (max(line_eq['a'], normal_eq['a'])%min(line_eq['a'], normal_eq['a']) == 0 and
         max(line_eq['b'], normal_eq['b'])%min(line_eq['b'], normal_eq['b']) == 0):
-        True
+        return True
     else:
-        False
+        return False
